43259dc5b5e046bc815ba481752a08abf7c92b70
Athi_Nair_2000 <athinair2000@gmail.com>
2019-05-26 23:33:13 +0530

    Problems related to concept of methods part 3

---
+++ Ch42.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ch42{
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args){
+	int k;
+	k=sc.nextInt();
+	do{
+		System.out.println(k);
+		k++;
+	}while(k<10);
+	}
+}
+	
